bb8184192a04b46ec0d7225659e8102bf529451c
Shrenik Jain <shrenikkjain81@gmail.com>
2024-07-31 15:37:29 +0530

    add new particles JS

---
+++ assets/js/bubbles.js
@@ -0,0 +1,59 @@
+particlesJS(
+    "dots", {
+    "particles": {
+      "number": {
+        "value": 250
+      },
+      "color": {
+        "value": "#eee"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.2,
+        "random": true,
+        "anim": {
+          "enable": false
+        }
+      },
+      "size": {
+        "value": 6,
+        "random": true,
+        "anim": {
+          "enable": false,
+        }
+      },
+      "line_linked": {
+        "enable": false
+      },
+      "move": {
+        "enable": true,
+        "speed": 3,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out"
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "push": {
+          "particles_nb": 10
+        }
+      }
+    },
+    "retina_detect": true
+  });
+  
